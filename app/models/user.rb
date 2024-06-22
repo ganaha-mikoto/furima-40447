b62fbf40a2ebbe0d6d8d96dd875fc5a713a54b9c
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-22 22:01:00 +0900

    バリデーションの設定
     メールアドレスは、@を含む必要があること。
     パスワードは、6文字以上での入力が必須であること
     パスワードは、半角英数字混合での入力が必須であること
    新規登録とログインのビュー

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
+  has_many :products, dependent: :destroy
+  has_many :histories
 end
