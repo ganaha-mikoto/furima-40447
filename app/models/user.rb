50fd29a80c9d5ac5728695cbcac49d401ef1d39a
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-06-23 20:53:28 +0900

    新規登録のバリデーション
    パスワードのバリデーションの記載を削除
    コントローラーのdevise_parameter_sanitizer.permitの編集
    ログインの有無で表示が変わるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :encrypted_password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :encrypted_password, presence: true, length: { minimum: 6 }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
