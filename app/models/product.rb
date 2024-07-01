583cbbc311da65bc7644fc27dafffdb9bc04a389
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-07-02 03:00:35 +0900

    商品の並び順とその他
    商品の並びを新しい方が後に来るようにした
    ヒストリーテーブルとのアソシエーションを編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # app/models/product.rb
 class Product < ApplicationRecord
   belongs_to :user
-  belongs_to :history
+  has_one :history
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
@@ -14,5 +14,4 @@ class Product < ApplicationRecord
   validates :name, :description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price, presence: true
   validates :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "must be between 300 and 9999999" }
-  validates :image, presence: true
 end
