780ee23db1e8002cd47fbdf4465c689ce67c35d5
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-07-03 14:54:49 +0900

    詳細表示機能
    ルーティング・コントローラーの設定
    まだ設定していない機能へのリンクの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def index
     @products = Product.order(created_at: :desc)
@@ -17,9 +18,30 @@ class ProductsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def show
+  end
+
+  def edit
+    # Ensure the current user is the owner of the product
+    redirect_to root_path unless @product.user == current_user
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price, :image ).merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
