167b55db9cbede3bddf690fa9c849c7a7671bac0
ganaha-mikoto <mosimosimikotodesu@icloud.com>
2024-07-20 10:04:51 +0900

    初期値がある場合は表示

--- app/javascript/product_price.js
+++ app/javascript/product_price.js
@@ -1,10 +1,23 @@
 // 関数として定義
 function calculatePrice() {
   const priceInput = document.getElementById("item-price");
+  const inputValue = priceInput ? priceInput.value : null;
 
   if (priceInput) {
     priceInput.addEventListener("input", () => {
       const inputValue = priceInput.value;
+      updatePrice(inputValue);
+    });
+
+    // 初期値がある場合には表示を更新する
+    if (inputValue) {
+      updatePrice(inputValue);
+    }
+  }
+}
+
+// 価格更新関数を定義
+function updatePrice(inputValue) {
       // 販売手数料を計算
       const fee = Math.floor(inputValue * 0.1);
       
@@ -17,8 +30,6 @@ function calculatePrice() {
 
       const profitDom = document.getElementById("profit");
       profitDom.innerHTML = profit;
-    });
-  }
 }
 
 // turbo:loadとturbo:renderイベントで関数を呼び出し
